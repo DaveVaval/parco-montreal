4aa9b660a1c172a3cd70ba3c993ff7836256d44e
DaveVaval <grimmjowx9@hotmail.com>
2024-10-21 17:38:16 -0400

    Rough draft of home screen done, added rnr, now need to add other routes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,10 @@
+import '../global.css'
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import Drawer from 'expo-router/drawer';
 import { Ionicons } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
@@ -59,6 +61,9 @@ export default function RootLayout() {
             name='index'
             options={{
               title: 'Payez une place',
+              drawerIcon: () => (
+                <MaterialIcons name='local-parking' size={20} color={colorScheme === 'dark' ? 'white' : 'blue'}/>
+              ),
               headerRight: () => (
                 <View style={{marginRight: 10}}>
                   <Ionicons name='settings-outline' size={25} color={colorScheme === 'dark' ? 'white' : 'blue'}/>
--- app/index.tsx
+++ app/index.tsx
@@ -2,38 +2,76 @@ import {
   StyleSheet,
   TouchableOpacity,
   View,
-  Text
+  Text,
+  Keyboard,
+  Pressable,
+  Image
 } from 'react-native'
 import { useRef, useEffect } from 'react'
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import LottieView from 'lottie-react-native';
 import { FontAwesome6 } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
+import { Input } from '@/components/ui/input';
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from '@/components/ui/card';
 
 export default function index() {
   const animation = useRef<LottieView>(null);
 
+
+
   return (
-    <ThemedView style={styles.container}>
-      {/* <LottieView
-        autoPlay
-        ref={animation}
-        style={{
-          width: 200,
-          height: 200
-        }}
-        source={require('@/assets/animations/parking-animation.json')}
-      /> */}
-      {/* <ThemedText>home</ThemedText> */}
-      <View style={{backgroundColor: 'white'}}>
-        <FontAwesome6 name='square-parking' size={100} color="#47a3ff"/>
-      </View>
-      <TouchableOpacity style={{width: '100%'}}>
-        <View style={styles.button}>
-          <Text style={{color: 'white', fontWeight: 500}}>Rechercher</Text>
+    <Pressable className='flex-1' onPress={Keyboard.dismiss}>
+      <ThemedView style={styles.container}>
+        <View className='justify-around items-center w-full h-[90%]'>
+          <Card className='w-full max-w-sm'>
+            <CardHeader>
+              <CardTitle>Place récente</CardTitle>
+            </CardHeader>
+            <CardContent className='items-center'>
+              <Image
+                source={require('@/assets/images/1159425.png')}
+                style={{
+                  height: 300,
+                  width: 300
+                }}
+              />
+            </CardContent>
+            <CardFooter className='justify-between items-center'>
+              <ThemedText className='font-semibold'>PN697</ThemedText>
+              <TouchableOpacity>
+                <Ionicons name='information-circle' color={'white'} size={25}/>
+              </TouchableOpacity>
+            </CardFooter>
+          </Card>
+          <View className='w-full'>
+            <View className='flex-row w-full justify-center items-center mb-2'>
+              <Input
+                className='w-[70%] mr-2'
+                placeholder='Entrez le code'
+              />
+              <TouchableOpacity>
+                <View className='bg-blue h-14 w-14 ml-2 items-center justify-center rounded-lg'>
+                  <FontAwesome6 name='camera' size={20} color="white"/>
+                </View>
+              </TouchableOpacity>
+            </View>
+            <TouchableOpacity className='w-full mt-2'>
+              <View style={styles.button}>
+                <Text style={{color: 'white', fontWeight: 500}}>Rechercher</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
         </View>
-      </TouchableOpacity>
-    </ThemedView>
+      </ThemedView>
+    </Pressable>
   )
 }
 
