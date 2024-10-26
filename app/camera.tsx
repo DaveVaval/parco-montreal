346f6a768fc24a00852408ddbfec017e4b711a57
DaveVaval <grimmjowx9@hotmail.com>
2024-10-26 19:11:45 -0400

    Added text recognition to vision-camera, need to a skia render

--- app/camera.tsx
+++ app/camera.tsx
@@ -1,10 +1,47 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Permission, StyleSheet } from 'react-native'
+import { useEffect, useState } from 'react'
+import { 
+  Camera, 
+  useCameraPermission, 
+  useCameraDevice,
+  useFrameProcessor,
+  useSkiaFrameProcessor
+} from 'react-native-vision-camera';
+import { useTextRecognition } from 'react-native-vision-camera-text-recognition' 
 
-export default function Camera() {
+export default function CameraScreen() {
+  const device = useCameraDevice('back');
+  const { hasPermission, requestPermission } = useCameraPermission();
+  const [data, setData] = useState<any>();
+
+  const { scanText } = useTextRecognition();
+
+  const frameProcessor = useFrameProcessor((frame) => {
+    'worklet'
+    const data = scanText(frame)
+    console.log(JSON.stringify(data[0].resultText, null, 2));
+  }, []);
+
+  const skiaFrameProcessor = useSkiaFrameProcessor((frame) => {
+    'worklet'
+    frame.render();
+  },[])
+
+  if (!hasPermission) {
+    console.log('ping')
+    requestPermission()
+  }
+
+  const logText = (data: any) => {
+    // console.log(data)
+  }
+
   return (
-    <View>
-      <Text>Camera</Text>
-    </View>
+    <Camera
+      style={StyleSheet.absoluteFill}
+      frameProcessor={frameProcessor}
+      device={device!}
+      isActive
+    />
   )
 }
