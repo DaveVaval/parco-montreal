41a6f35117b2e44921e80b8daac31b8b4cb2a193
DaveVaval <grimmjowx9@hotmail.com>
2024-10-31 16:38:50 -0400

    working on history screen, integrated flashlist

--- app/history.tsx
+++ app/history.tsx
@@ -1,10 +1,30 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { ThemedText } from '@/components/ThemedText'
+import { ThemedView } from '@/components/ThemedView'
+import { FlashList } from '@shopify/flash-list'
+
+const DATA = [
+  {
+    title: "First Item",
+  },
+  {
+    title: "Second Item",
+  },
+];
 
 export default function History() {
   return (
-    <View>
-      <Text>History</Text>
-    </View>
+    <ThemedView className='flex-1'>
+      <FlashList
+        data={DATA}
+        renderItem={({ item }) => (
+          <View>
+            <ThemedText>{item.title}</ThemedText>
+          </View>
+        )}
+        estimatedItemSize={200}
+      />
+    </ThemedView>
   )
 }
